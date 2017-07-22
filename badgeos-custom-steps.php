54229fec75ca159ef3b0326fdfdbd0612157f6c6
Thomas Pointon <thomas.pointon16@imperial.ac.uk>
2017-07-22 15:11:05 +0100

    Streamlined initialisation code

--- badgeos-custom-steps.php
+++ badgeos-custom-steps.php
@@ -1,62 +1,100 @@
 <?php
-/*
-Plugin Name: BadgeOS Custom Steps JS Triggers
-Description: A BadgeOS Plugin giving "Required Steps" the ability to listen for JS function calls.
-Version: 0.0.2
-Author: Teepee Studios
-Author URI: http://www.teepeestudios.com
-*/
-
-class BadgeOS_CustomSteps {
-
-  function __construct() {
-
-    // Plugin Constants
-    $this->basename = plugin_basename( __FILE__ );
-    $this->directory_path = plugin_dir_path( __FILE__ );
-    $this->directory_url = plugin_dir_url( __FILE__ );
-
-    // Dependency check
-    add_action( 'admin_notices', array( $this, 'check_plugin_dependencies' ) );
-
-    // Load plugin files
-    add_action( 'plugins_loaded', array( $this, 'load_plugin_files' ), 11 );
-
-  }
-
-}
+/**
+ * Plugin Name: BadgeOS Custom JS Steps
+ * Description: This BadgeOS add-on gives "Required Steps" the ability to listen for a JS function call.
+ * Author: Teepee Studios
+ * Version: 0.0.1
+ * Author URI: http://www.teepeestudios.com/
+ */
 
 /**
-  * Deactivates the plugin with an error message if dependencies are not met (BadgeOS not installed)
-  */
-public function check_plugin_dependencies() {
+ * The main plugin instantiation class
+ *
+ * This contains important things that our relevant to
+ * the add-on running correctly. Things like registering
+ * custom post types, taxonomies, posts-to-posts
+ * relationships, and the like.
+ *
+ * @since 0.0.1
+ */
+class badgeos_custom_js_steps {
 
-  // BadgeOS
-  if ( !class_exists( 'BadgeOS' ) || !function_exists( 'badgeos_get_user_earned_achievement_types' ) ) {
+	/**
+	 * Get everything running.
+	 *
+	 * @since 0.0.1
+	 */
+	function __construct() {
 
-    // Admin error message
-    echo '<div id="message" class="error">';
-    if ( !class_exists( 'BadgeOS' ) || !function_exists( 'badgeos_get_user_earned_achievement_types' ) ) {
-      echo '<p>' . sprintf( __( 'BadgeOS Custom Steps Add-On requires BadgeOS and has been <a href="%s">deactivated</a>. Please install and activate BadgeOS and then reactivate this plugin.', 'badgeos-custom-steps' ), admin_url( 'plugins.php' ) ) . '</p>';
-    }
-    echo '</div>';
+		// Define plugin constants
+		$this->basename       = plugin_basename( __FILE__ );
+		$this->directory_path = plugin_dir_path( __FILE__ );
+		$this->directory_url  = plugins_url( dirname( $this->basename ) );
 
-    // Deactivate plugin
-    deactivate_plugins( $this->basename );
-  }
-}
+		// If BadgeOS is unavailable, deactivate the plugin
+		add_action( 'admin_notices', array( $this, 'maybe_disable_plugin' ) );
 
-/**
-  * Loads the plugin files
-  */
-public function load_plugin_files() {
+		// Include the other plugin files
+		add_action( 'init', array( $this, 'includes' ) );
 
+	} /* __construct() */
+
+
+	/**
+	 * Include the plugin dependencies
+	 *
+	 * @since 0.0.1
+	 */
+	public function includes() {
+
+		// If BadgeOS is available...
 		if ( $this->meets_requirements() ) {
-			require_once( $this->directory_path . '/includes/rules-engine.php' );
-			require_once( $this->directory_path . '/includes/steps-ui.php' );
 
-			$this->action_forwarding();
+			// Set up the trigger(s)
+			require_once( $this->directory_path . '/includes/trigger.php' );
+
 		}
-	}
 
-$GLOBALS[ 'badgeos_customsteps' ] = new BadgeOS_CustomSteps();
+	} /* includes() */
+
+	/**
+	 * Check if BadgeOS is available
+	 *
+	 * @since 0.0.1
+	 * @return bool True if BadgeOS is available, false otherwise
+	 */
+	public static function meets_requirements() {
+
+		if ( class_exists('BadgeOS') )
+			return true;
+		else
+			return false;
+
+	} /* meets_requirements() */
+
+	/**
+	 * Potentially output a custom error message and deactivate
+	 * this plugin, if we don't meet requriements.
+	 *
+	 * This fires on admin_notices.
+	 *
+	 * @since 0.0.1
+	 */
+	public function maybe_disable_plugin() {
+
+		if ( ! $this->meets_requirements() ) {
+			// Display the error
+			echo '<div id="message" class="error">';
+			echo '<p>' . sprintf( __( 'BadgeOS Custom JS Steps requires BadgeOS and has been <a href="%s">deactivated</a>. Please install and activate BadgeOS and then reactivate this plugin.', 'badgeos-custom-js-steps' ), admin_url( 'plugins.php' ) ) . '</p>';
+			echo '</div>';
+
+			// Deactivate the plugin
+			deactivate_plugins( $this->basename );
+		}
+
+	} /* maybe_disable_plugin() */
+
+} /* badgeos_custom_js_steps */
+
+// Instantiate the class to a global variable that we can access elsewhere
+$GLOBALS['badgeos_custom_js_steps'] = new badgeos_custom_js_steps();
